a4613c033596a6436ec9616e3ce8a03e4182d9e5
gavelprashant99 <prashantgavel99@gmail.com>
2024-11-06 00:20:14 +0530

    Finished Project

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,31 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import projecticon from '../../assets/projecticon.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={projecticon}
+          link="https://sws.delhi.gov.in/home"
+          h3="Single Window System"
+          p="Digital Platform"
+        />
+        <ProjectCard
+          src={projecticon}
+          link="https://cguadfinance.in/"
+          h3="SUDA"
+          p="Hamburger Restaurant"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
